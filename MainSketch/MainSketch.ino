748373788d4c363de8a458d0076ed72db6ff11c5
Ayesh Jayasekara <ejkpac@gmail.com>
2017-04-15 16:29:18 +0530

    Update
    Routine Commit

--- MainSketch/MainSketch.ino
+++ MainSketch/MainSketch.ino
@@ -2,7 +2,7 @@
   Pubnub Subscribe 
 *********************************************************************************/
 #include <ESP8266WiFi.h>
-
+#include "ShiftRegister74HC595.h"
 const char* g_ssid       = "Dialog 4G";
 const char* g_password   = "soclab@1";
 const char* g_host       = "pubsub.pubnub.com";
@@ -10,8 +10,24 @@ const char* g_pubKey     = "pub-c-784c01b6-34fa-46e2-aba7-5eebb8efac0e";
 const char* g_subKey     = "sub-c-764a7b9e-16a3-11e7-bb8a-0619f8945a4f";
 const char* g_channel    = "kozi";
 String      timeToken    = "0";
+int arr[24];
 bool decodePending = true;
 
+
+//int SER_Pin = 5;   //pin 14 on the 75HC595
+//int RCLK_Pin = 4;  //pin 12 on the 75HC595
+//nt SRCLK_Pin = 0; //pin 11 on the 75HC595
+/*
+  ShiftRegister74HC595.h - Library for easy control of the 74HC595 shift register.
+  Created by Timo Denk (www.simsso.de), Nov 2014.
+  Additional information are available on http://shiftregister.simsso.de/
+  Released into the public domain.
+*/
+
+
+
+// create shift register object (number of shift registers, data pin, clock pin, latch pin)
+ShiftRegister74HC595 sr (3, 5, 4, 0); 
 
 
 void setup()
@@ -34,12 +50,16 @@ void setup()
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
+  for(int i = 0 ; i < 24 ; i++)
+  arr[i]=0;
+  sr.setAllLow();
 }
 
 
 void loop()
 {
+   // set all pins LOW
+  delay(500); 
   WiFiClient client;
   const int l_httpPort = 80;
   if (!client.connect(g_host, l_httpPort))
@@ -76,6 +96,21 @@ void loop()
   }
   Serial.println("closing connection");
   delay(200);
+
+   
+  //sr.set(5,HIGH);
+  //delay(1000);
+//  for (int i = 0; i < 24; i++) {
+//    
+//    sr.set(i, HIGH); // set single pin HIGH
+//    delay(1250); 
+//  }
+//    for (int i = 0; i < 24; i++) {
+//    
+//    sr.set(i,LOW); // set single pin HIGH
+//    delay(1250); 
+//  }
+  
 }
 
 void getNextTimeToken(String l){
@@ -97,6 +132,35 @@ void decodeLine(String l){
       l = l.substring(4,l.indexOf(',')-2);
       Serial.println(l);
       delay(100);
+      applyChanges(l);
   }
+
+void applyChanges(String msg){
+  for(int i =0 ; i < msg.length() ; i ++)
+        if(msg.charAt(i)=='0')
+          sr.set(i,LOW);
+        else
+          sr.set(i,HIGH);
+   //sr.setAllHigh();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ MainSketch/README.md
@@ -0,0 +1,3 @@
+# Shift-Register-74HC595-Arduino-Library
+
+This is project done by a final year student for a home automation system. This project uses 74HC595 Shift Register to expand digital output pins. The whole project uses a NodeMCU board as the brain.
