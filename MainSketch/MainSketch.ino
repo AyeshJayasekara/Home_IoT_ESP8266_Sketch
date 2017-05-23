aa0c53ff1e6eb01905d8fabf1bb9f9e8c60c6555
Ayesh Jayasekara <ejkpac@gmail.com>
2017-05-24 02:59:24 +0530

    Updated

--- MainSketch/MainSketch.ino
+++ MainSketch/MainSketch.ino
@@ -3,44 +3,56 @@
 *********************************************************************************/
 #include <ESP8266WiFi.h>
 #include "VirtualDelay.h"
+#include <math.h>  
 VirtualDelay virtualDelay(millis);
 #include "ShiftRegister74HC595.h"
 const char* g_ssid       = "Dialog 4G";
-const char* g_password   = "*******";
+const char* g_password   = "soclab@123";
 const char* g_host       = "pubsub.pubnub.com";
 const char* g_pubKey     = "pub-c-89d87bfb-89a1-4ae9-941c-631dd1ea5dbd";
 const char* g_subKey     = "sub-c-e7c47d7a-13be-11e7-a9ec-0619f8945a4f";
 const char* g_channel    = "0";
 String      timeToken    = "0";
-int arr[24];
+double temp=0;
+int soil=0;
+int rain=0;
 bool decodePending = true;
 bool decodePending2 = true;
+WiFiClient client;
 
 
-//int SER_Pin = 5;   //pin 14 on the 75HC595
+//int SER_Pin = 5;   //pin 14 on the 75HC595 
 //int RCLK_Pin = 4;  //pin 12 on the 75HC595
 //nt SRCLK_Pin = 0; //pin 11 on the 75HC595
 /*
   ShiftRegister74HC595.h - Library for easy control of the 74HC595 shift register.
   Created by Timo Denk (www.simsso.de), Nov 2014.
   Additional information are available on http://shiftregister.simsso.de/
-  Released into the public domain.
+  Released into the public domain. kaludamalu
 */
 
 
 
 // create shift register object (number of shift registers, data pin, clock pin, latch pin)
-ShiftRegister74HC595 sr (5, 5, 4, 0); 
+ShiftRegister74HC595 sr (4, 5, 4, 0); 
 
-
+void GET();
+void POST();
+void getNextTimeToken(String l);
+void readTemp();
+void readRain();
+void readSoil();
+void decodeLine(String l);
+void applyChanges(String msg);
+void setDisplay(int T);
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(g_ssid);
-
+  
   WiFi.begin(g_ssid, g_password);
 
   while (WiFi.status() != WL_CONNECTED)
@@ -53,10 +65,7 @@ void setup()
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  for(int i = 0 ; i < 24 ; i++)
-  arr[i]=0;
   sr.setAllLow();
-  setDigit();
 }
 
 
@@ -66,29 +75,16 @@ void loop()
   //DATA FORMAT : http://pubsub.pubnub.com /publish/pub-key/sub-key/signature/channel/callback/message
   //SUB FORMAT :  http://pubsub.pubnub.com /subscribe/sub-key/channel/callback/timetoken
   //if(virtualDelay.done(1000))
-  GET();
-
-  //Serial.println(analogRead(A0));  
-  //sr.set(5,HIGH);
-  //delay(1000);
-//  for (int i = 0; i < 24; i++) {
-//    
-//    sr.set(i, HIGH); // set single pin HIGH
-//    delay(1250); 
-//  }
-//    for (int i = 0; i < 24; i++) {
-//    
-//    sr.set(i,LOW); // set single pin HIGH
-//    delay(1250); 
-//  }
-  
-}
-void setDigit(){
-  int arr[4] = {0,0,0,0};
-  sr.set(37,LOW);
-  sr.set(38,HIGH);
-  sr.set(39,LOW);
-  sr.set(40,LOW);
+ readTemp();
+ readSoil();
+ readRain();
+ POST();
+ delay(1000);
+   GET();
+if(temp>33)
+  sr.set(7,HIGH);
+  else
+  sr.set(7,LOW);
   
 }
 
@@ -115,18 +111,15 @@ void decodeLine(String l){
   }
 
 void applyChanges(String msg){
-  for(int i =0 ; i < msg.length() ; i ++)
+  for(int i =0 ; i < 8 ; i ++)
+    //Serial.println(msg.charAt(i));
         if(msg.charAt(i)=='0')
           sr.set(i,LOW);
-        else
+          else
           sr.set(i,HIGH);
-          if(msg.charAt(0)=='1')
-          POST();
-   //sr.setAllHigh();
 }
 
 void POST(){
-  WiFiClient client;
   const int l_httpPort = 80;
   String puburl = "/publish/";
   puburl += g_pubKey;
@@ -138,12 +131,20 @@ void POST(){
  // puburl += '"';
   puburl += "/0/";
   puburl += '"';
-  puburl += "Ayesh";
+  puburl += (int)temp;
+  puburl += '!';
+  puburl += soil;
+  puburl += '!';
+  puburl += rain;
+  puburl += '!';
   puburl += '"';
-  if (!client.connect(g_host, l_httpPort))
+if (!client.connect("pubsub.pubnub.com", 80))
   {
     Serial.println("connection failed");
     return;
+  }
+  else{
+    Serial.println("Connected to PUBNUB PUBLISH");
   }
   client.print(String("GET ") + puburl + " HTTP/1.1\r\n" +
                "Host: " + g_host + "\r\n"+
@@ -168,12 +169,15 @@ void GET(){
   url += g_channel;
   url += "/0/";
   url += timeToken;
-  WiFiClient client;
+  //WiFiClient client;
   const int l_httpPort = 80;
-  if (!client.connect(g_host, l_httpPort))
+if (!client.connect("pubsub.pubnub.com", 80))
   {
     Serial.println("connection failed");
     return;
+  }
+  else{
+    Serial.println("Connected to PUBNUB");
   }
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + g_host + "\r\n"+
@@ -193,6 +197,161 @@ void GET(){
 client.stop();
 client.flush();
 }
+
+void readTemp(){
+  int pretemp = temp;
+  sr.set(13,LOW);
+  sr.set(14,LOW);
+  sr.set(15,LOW);
+  delay(100);
+  temp=analogRead(A0);
+  //temp=temp-1024;
+  temp= ((1024-temp)/1024.0 -0.5)*100;
+  if(pretemp != (int)temp)
+  setDisplay(temp);
+}
+void readSoil(){
+  sr.set(13,LOW);
+  sr.set(14,LOW);
+  sr.set(15,HIGH);
+  delay(100);
+  soil=analogRead(A0);
+}
+void readRain(){
+  sr.set(13,LOW);
+  sr.set(14,HIGH);
+  sr.set(15,LOW);
+  delay(100);
+  rain=analogRead(A0);
+}
+ void setDisplay(int T){
+    int a = T/10;
+    int b = T%10;
+    switch(b){
+      case 0:
+      sr.set(28,LOW);
+      sr.set(29,LOW);
+      sr.set(30,LOW);
+      sr.set(31,LOW);
+      break;
+      case 1:
+      sr.set(28,HIGH);
+      sr.set(29,LOW);
+      sr.set(30,LOW);
+      sr.set(31,LOW);
+      break;
+      case 2:
+      sr.set(28,LOW);
+      sr.set(29,HIGH);
+      sr.set(30,LOW);
+      sr.set(31,LOW);
+      break;
+      case 3:
+      sr.set(28,HIGH);
+      sr.set(29,HIGH);
+      sr.set(30,LOW);
+      sr.set(31,LOW);
+      break;
+      case 4:
+      sr.set(28,LOW);
+      sr.set(29,LOW);
+      sr.set(30,HIGH);
+      sr.set(31,LOW);
+      break;
+      case 5:
+      sr.set(28,HIGH);
+      sr.set(29,LOW);
+      sr.set(30,HIGH);
+      sr.set(31,LOW);
+      break;
+      case 6:
+      sr.set(28,LOW);
+      sr.set(29,HIGH);
+      sr.set(30,HIGH);
+      sr.set(31,LOW);
+      break;
+      case 7:
+      sr.set(28,HIGH);
+      sr.set(29,HIGH);
+      sr.set(30,HIGH);
+      sr.set(31,LOW);
+      break;
+      case 8:
+      sr.set(28,LOW);
+      sr.set(29,LOW);
+      sr.set(30,LOW);
+      sr.set(31,HIGH);
+      break;
+      case 9:
+      sr.set(28,HIGH);
+      sr.set(29,LOW);
+      sr.set(30,LOW);
+      sr.set(31,HIGH);
+      break;
+    }
+    switch(a){
+      case 0:
+      sr.set(24,LOW);
+      sr.set(25,LOW);
+      sr.set(26,LOW);
+      sr.set(27,LOW);
+      break;
+      case 1:
+      sr.set(24,HIGH);
+      sr.set(25,LOW);
+      sr.set(26,LOW);
+      sr.set(27,LOW);
+      break;
+      case 2:
+      sr.set(24,LOW);
+      sr.set(25,HIGH);
+      sr.set(26,LOW);
+      sr.set(27,LOW);
+      break;
+      case 3:
+      sr.set(24,HIGH);
+      sr.set(25,HIGH);
+      sr.set(26,LOW);
+      sr.set(27,LOW);
+      break;
+      case 4:
+      sr.set(24,LOW);
+      sr.set(25,LOW);
+      sr.set(26,HIGH);
+      sr.set(27,LOW);
+      break;
+      case 5:
+      sr.set(24,HIGH);
+      sr.set(25,LOW);
+      sr.set(26,HIGH);
+      sr.set(27,LOW);
+      break;
+      case 6:
+      sr.set(24,LOW);
+      sr.set(25,HIGH);
+      sr.set(26,HIGH);
+      sr.set(27,LOW);
+      break;
+      case 7:
+      sr.set(24,HIGH);
+      sr.set(25,HIGH);
+      sr.set(26,HIGH);
+      sr.set(27,LOW);
+      break;
+      case 8:
+      sr.set(24,LOW);
+      sr.set(25,LOW);
+      sr.set(26,LOW);
+      sr.set(27,HIGH);
+      break;
+      case 9:
+      sr.set(24,HIGH);
+      sr.set(25,LOW);
+      sr.set(26,LOW);
+      sr.set(27,HIGH);
+      break;
+    }
+ }
 
 
 
